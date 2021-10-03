3a28d6b5200487489cc0db2bee19561e0a8f15be
Allan Taylor <allan@startmail.com>
2021-10-02 21:14:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,53 +14,7 @@ resource "null_resource" "configure_cert_manager" {
       kubectl create namespace ${var.cert_manager_namespace}
       helm repo add jetstack https://charts.jetstack.io
       helm repo update
-      helm install cert-manager jetstack/cert-manager \
-        --namespace ${var.cert_manager_namespace} \
-        --version ${var.cert_manager_version} \
-        --set installCRDs=true \
-        --set image.repository=${var.image_path} \
-        --set image.tag=${var.image_tag} \
-        --set image.replicaCount=${var.replicas} \
-        --set global.imagePullSecrets[0].name="artifactory-access" \
-        --set serviceAccount.name=${var.service_account_name} \
-        --set webhook.image.repository=${var.webhook_image_path} \
-        --set webhook.image.tag=${var.webhook_image_tag} \
-        --set webhook.serviceType="ClusterIP" \
-        --set cainjector.image.repository=${var.cainjector_image_path} \
-        --set cainjector.image.tag=${var.cainjector_image_tag}    
-    EOT
-  }
-}
-
-resource "null_resource" "configure_cert_manager_vault_issuer" {
-  depends_on = [null_resource.configure_cert_manager]
-  provisioner "local-exec" {
-    command = <<-EOT
-      b64token=$(echo "$VAULT_TOKEN" | base64)
-      cat <<EOF | kubectl apply -f -
-      apiVersion: v1
-      kind: Secret
-      type: Opaque
-      metadata:
-        name: cert-manager-vault-token
-        namespace: cert-manager
-      data:
-        token: $b64token
-      ---         
-      apiVersion: cert-manager.io/v1
-      kind: ClusterIssuer
-      metadata:
-        name: vault-issuer
-      spec:
-        vault:
-          path: ${var.vault_issuer_path}
-          server: $VAULT_ADDR
-          caBundle: ${var.vault_tls_cert_ca}
-          auth:
-            tokenSecretRef:
-                name: cert-manager-vault-token
-                key: token
-      EOF
+      helm install cert-manager jetstack/cert-manager --namespace ${var.cert_manager_namespace} --values cert-manager-values.yaml
     EOT
   }
 }
