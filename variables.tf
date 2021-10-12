9dd3eb5276926f7cf10442d390105ae88bda0299
Allan Taylor <allan@startmail.com>
2021-10-12 12:27:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "cert_manager_namespace" {
     type = string
     description = "The namespace cert-manager will be deployed to."
 }
+
+variable "vault_token" {
+    type = string
+    description = "The token used to access Vault."
+}
