e0ad9a6561b09916ac3a2ae7ff5ed9354d1f11c3
Allan Taylor <allan@startmail.com>
2022-03-19 07:48:43 -0400

    wip

--- variables.tf
+++ variables.tf
@@ -6,29 +6,35 @@ variable "cert_manager_namespace" {
 variable "enable_vault_issuer" {
     type = bool
     description = "Enables a Hashi Vault issuer."
+    default = false
 }
 
 variable "cert_manager_secret_id" {
     type = string
     description = "The secret id used in the Vault app role."
+    default = null
 }
 
 variable "cert_manager_role_id" {
     type = string
     description = "The role id used in the Vault app role."
+    default = null
 }
 
 variable "vault_ca_bundle" {
     type = string
     description = "The CA bundle file where Vault is the root or intermediate CA."
+    default = null
 }
 
 variable "vault_path" {
     type = string
     description = "The path to the Vault PKI engine."
+    default = null
 }
 
 variable "vault_server" {
     type = string
     description = "The Vault server URL."
+    default = null
 }
